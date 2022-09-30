60a64a2f63646ae369f81771bff0f650a4529c55
Jezer Mejía <jezer.mejia13523@est.uca.edu.ni>
2022-09-29 21:56:14 -0600

    Removed unnecessary code. There is no need for Parcelable implementation in Product class

--- app/src/main/java/com/jezerm/listadoprod/dataclass/Product.kt
+++ app/src/main/java/com/jezerm/listadoprod/dataclass/Product.kt
@@ -3,35 +3,8 @@ package com.jezerm.listadoprod.dataclass
 import android.os.Parcel
 import android.os.Parcelable
 
-class Product(var id: Int, var name: String, var price: Double): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString() ?: "",
-        parcel.readDouble()
-    ) {
-    }
-
+class Product(var id: Int, var name: String, var price: Double) {
     override fun toString(): String {
         return "{ ID: $id, Name: $name, Price: $price }"
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(name)
-        parcel.writeDouble(price)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Product> {
-        override fun createFromParcel(parcel: Parcel): Product {
-            return Product(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Product?> {
-            return arrayOfNulls(size)
-        }
     }
 }
